80f0f70f0b5576d2dd323268c5309085691c80a7
Rp-sushil <sushil.18je0861@am.iitism.ac.in>
2020-10-24 23:57:04 +0530

    react and web app

--- digital-clock-with-react/src/App.test.js
+++ digital-clock-with-react/src/App.test.js
@@ -22,7 +22,7 @@ describe("Should have a clock", () => {
 })
 
 
-describe("Clock is Running", () => {
+describe("Clock is Running1", () => {
   it("should increment time by 1sec after 1 second", () => {
     jest.useFakeTimers('modern');
     const component = shallow(<App />);
@@ -39,16 +39,86 @@ describe("Clock is Running", () => {
     var ns = parseInt(new_time.slice(new_time.lastIndexOf(':') + 1, new_time.indexOf(' ')));
     var npm = new_time.indexOf('P');
     var flag = false;
-    if(cm == 59 && cs == 59){
-      if(ch == 11){
-        flag = (nh == 12 && nm == 0 && ns == 0 && cpm != npm);
-      } else{
-        flag = (nh == ch + 1 && nm == 0 && ns == 0 && cpm == npm);
+    if(cm === 59 && cs === 59){
+      if(ch === 11){
+        flag = (nh === 12 && nm === 0 && ns === 0 && cpm != npm);
+      } 
+      if(ch === 12){
+        flag = (nh === 1 && nm === 0 && ns === 0 && cpm === npm);
+      } 
+      else{
+        flag = (nh === ch + 1 && nm === 0 && ns === 0 && cpm === npm);
       }
-    } else if(cs == 59){
-      flag = (nh == ch && nm == cm + 1 && ns == 0 && cpm == npm);
+    } else if(cs === 59){
+      flag = (nh === ch && nm === cm + 1 && ns === 0 && cpm === npm);
     } else{
-      flag = (nh == ch && nm == cm && ns == cs + 1 && cpm == npm);
+      flag = (nh === ch && nm === cm && ns === cs + 1 && cpm === npm);
+    }
+    expect(flag).toBe(true);
+    jest.useRealTimers();
+  });
+})
+
+
+describe("Clock is Running2", () => {
+  it("should increment time by 1min after 1 minute", () => {
+    jest.useFakeTimers('modern');
+    const component = shallow(<App />);
+    const curr_time= component.find('#time').text();
+    component.instance().tick();
+    jest.advanceTimersByTime(60000);
+    const new_time = component.find('#time').text();
+    var ch = parseInt(curr_time.slice(0, curr_time.indexOf(':')));
+    var cm = parseInt(curr_time.slice(curr_time.indexOf(':')+1, curr_time.lastIndexOf(':')));
+    var cs = parseInt(curr_time.slice(curr_time.lastIndexOf(':') + 1, curr_time.indexOf(' ')));
+    var cpm = curr_time.indexOf('P');
+    var nh = parseInt(new_time.slice(0, new_time.indexOf(':')));
+    var nm = parseInt(new_time.slice(new_time.indexOf(':') + 1, new_time.lastIndexOf(':')));
+    var ns = parseInt(new_time.slice(new_time.lastIndexOf(':') + 1, new_time.indexOf(' ')));
+    var npm = new_time.indexOf('P');
+    var flag = false;
+    if(cm === 59){
+      if(ch === 11){
+        flag = (nh === 12 && nm === 0 && ns === cs && cpm != npm);
+      } 
+      else if(ch === 12){
+        flag = (nh === 1 && nm === 0 && ns === cs && cpm === npm);
+      }
+      else{
+        flag = (nh === ch + 1 && nm === 0 && ns === cs && cpm === npm);
+      }
+    } else{
+      flag = (nh === ch && nm === cm + 1 && ns === cs && cpm === npm);
+    }
+    expect(flag).toBe(true);
+    jest.useRealTimers();
+  });
+})
+
+describe("Clock is Running3", () => {
+  it("should increment time by 1min after 1 minute", () => {
+    jest.useFakeTimers('modern');
+    const component = shallow(<App />);
+    const curr_time= component.find('#time').text();
+    component.instance().tick();
+    jest.advanceTimersByTime(3600000);
+    const new_time = component.find('#time').text();
+    var ch = parseInt(curr_time.slice(0, curr_time.indexOf(':')));
+    var cm = parseInt(curr_time.slice(curr_time.indexOf(':')+1, curr_time.lastIndexOf(':')));
+    var cs = parseInt(curr_time.slice(curr_time.lastIndexOf(':') + 1, curr_time.indexOf(' ')));
+    var cpm = curr_time.indexOf('P');
+    var nh = parseInt(new_time.slice(0, new_time.indexOf(':')));
+    var nm = parseInt(new_time.slice(new_time.indexOf(':') + 1, new_time.lastIndexOf(':')));
+    var ns = parseInt(new_time.slice(new_time.lastIndexOf(':') + 1, new_time.indexOf(' ')));
+    var npm = new_time.indexOf('P');
+    var flag = false;
+    if(ch === 12){
+      flag = (nh === 1 && nm === cm && cs === ns && npm === cpm);
+    } else if(ch === 11){
+      flag = (nh === 12 && nm === cm && cs === ns && npm != cpm);
+    }
+    else{
+      flag = (nh === ch + 1 && nm === cm && cs === ns && npm === cpm);
     }
     expect(flag).toBe(true);
     jest.useRealTimers();
